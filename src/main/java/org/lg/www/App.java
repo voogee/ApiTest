94dd93bfe36cfd099936c246bd1282c016d82d4d
阿尔法（李刚） <ligang@soyoung.com>
2021-12-10 10:31:06 +0800

    add apitest

--- src/main/java/org/lg/www/App.java
+++ src/main/java/org/lg/www/App.java
@@ -22,6 +22,7 @@ public class App
     {
         List<String> jsonpathlist = new ArrayList();
         jsonpathlist.add("$.errorCode=1");
+        ApiTest apiTest= new ApiTest(1,"测试接口","m139.sy.soyoung.com","/mentorPlatform/actIndexInfo","uid=1714152120&xy_token=7c8306bd3b93f4b0f0a85338efa5f5b8&sys=1&lver=8.38.0&device_id=70178&mentor_id=10171&ab_id=44E4BC3957DC41265FF73548A35C30C7&city_id=0",jsonpathlist);
         ApiService apiService = new ApiService(apiTest);
 
     }
