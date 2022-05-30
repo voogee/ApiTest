aba07403faacf7db05880e37a34e2c6a5c87dd5a
阿尔法（李刚） <ligang@soyoung.com>
2022-05-30 18:08:03 +0800

    添加请求类

---
+++ src/main/java/com/voogee/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.voogee.controller;
+
+
+import com.voogee.common.HttpUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Scope("prototype")
+@RequestMapping("test")
+public class TestController {
+    @ResponseBody
+    @RequestMapping("test")
+    public String test(HttpServletRequest request){
+        String r=  HttpUtils.sendHttpGet("https://api8.soyoung.com/v8/misc/abtest");
+        return r;
+    }
+
+}
