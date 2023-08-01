761b8639b51787d9ac382dbdec4acdd68eac808b
home <voogee@163.com>
2023-08-01 22:16:18 +0800

    增加拦截器

--- src/main/java/com/voogee/mapper/UserMapper.java
+++ src/main/java/com/voogee/mapper/UserMapper.java
@@ -11,6 +11,8 @@ public interface UserMapper {
 
     public User login(User user);
     public List<User> getAllUser();
+
+    //mapper传参两种方式，不指定参数名xml则用#{下标获取}
 //    public User selone(@Param("username") String username);
     public User selone(String username);
 
